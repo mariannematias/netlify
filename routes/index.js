756117d829900783ce62b0c522dedb712a130d56
mariannematias <mariannec.matias@gmail.com>
2019-04-25 07:49:49 +0800

    add blog page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 const home = require('../pages/home.json')
+const blog = require('../pages/blog.json')
 
 /*  This is the home route. It renders the index.mustache page from the views directory.
 	Data is rendered using the Mustache templating engine. For more
@@ -13,5 +14,13 @@ router.get('/', (req, res) => {
 	}
 	res.render('index', data)
 })
+
+router.get('/blog', (req, res) => {
+	const data = {
+		page: blog
+	}
+	res.render('blog', data)
+})
+
 
 module.exports = router
