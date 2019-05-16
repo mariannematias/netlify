5baf98fd3be85a2965702e2f5b67cb4ed0169bbb
mariannematias <mariannec.matias@gmail.com>
2019-05-17 07:21:58 +0800

    layput updates

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $(".nav-search").click(function(){
         $(".form-inline").toggle();
     });
+
+    $("a.register-btn").click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 1200);
+    });
 });
